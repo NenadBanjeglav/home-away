fc1fed2939ef5d92c23577aa56cdd521b53424d6
NenadBanjeglav <nenadmobmail@gmail.com>
2024-08-08 20:05:20 +0200

    create property page inputs

--- app/rentals/create/page.tsx
+++ app/rentals/create/page.tsx
@@ -4,6 +4,11 @@ import FormInput from "@/components/form/FormInput";
 import FormContainer from "@/components/form/FormContainer";
 import { createPropertyAction } from "@/utils/actions";
 import SubmitButton from "@/components/form/Buttons";
+import PriceInput from "@/components/form/PriceInput";
+import CategoriesInput from "@/components/form/CategoriesInput";
+import TextAreaInput from "@/components/form/TextAreaInput";
+import CountriesInput from "@/components/form/CountriesInput";
+import ImageInput from "@/components/form/ImageInput";
 
 const CreatePropertyPage = () => {
   return (
@@ -27,6 +32,16 @@ const CreatePropertyPage = () => {
               label="Tagline (30 limit)"
               defaultValue="Dream Getaway Awaits You Here"
             />
+            <PriceInput />
+            <CategoriesInput />
+          </div>
+          <TextAreaInput
+            name="description"
+            labelText="Description (10 - 1000 words)"
+          />
+          <div className="mt-4 grid gap-8 sm:grid-cols-2">
+            <CountriesInput />
+            <ImageInput />
           </div>
           <SubmitButton text="create rental" className="mt-12" />
         </FormContainer>
