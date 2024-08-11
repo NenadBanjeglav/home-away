241200bfb865487779f299905aaa0b845197fa52
NenadBanjeglav <nenadmobmail@gmail.com>
2024-08-11 13:04:05 +0200

    emptyList component

--- components/home/EmptyList.tsx
+++ components/home/EmptyList.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
-const EmptyList = () => {
-  return <div>EmptyList</div>;
+const EmptyList = ({
+  heading = "No items in the list.",
+  message = "Keep exploring out properties",
+  btnText = "back home",
+}: {
+  heading?: string;
+  message?: string;
+  btnText?: string;
+}) => {
+  return (
+    <div className="mt-4">
+      <h2 className="text-xl font-bold">{heading}</h2>
+      <p className="text-lg">{message}</p>
+      <Button asChild className="mt-4 capitalize" size="lg">
+        <Link href="/">{btnText}</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default EmptyList;
