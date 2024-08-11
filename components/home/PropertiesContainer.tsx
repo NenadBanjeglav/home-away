3f99379eead739e963206e4b7a9db2787872f7d9
NenadBanjeglav <nenadmobmail@gmail.com>
2024-08-11 13:09:38 +0200

    propertiesContainer component

--- components/home/PropertiesContainer.tsx
+++ components/home/PropertiesContainer.tsx
@@ -1,13 +1,31 @@
 import React from "react";
 
-const PropertiesContainer = ({
+import { fetchProperties } from "@/utils/actions";
+import PropertiesList from "./PropertiesList";
+import EmptyList from "./EmptyList";
+import type { PropertyCardProps } from "@/utils/types";
+
+const PropertiesContainer = async ({
   category,
   search,
 }: {
   category?: string;
   search?: string;
 }) => {
-  return <div>PropertiesContainer</div>;
+  const properties: PropertyCardProps[] = await fetchProperties({
+    category,
+    search,
+  });
+  if (properties.length === 0) {
+    return (
+      <EmptyList
+        heading="No resutls."
+        message="Try chaning or removing some your filters."
+        btnText="Clear Filters"
+      />
+    );
+  }
+  return <PropertiesList properties={properties} />;
 };
 
 export default PropertiesContainer;
