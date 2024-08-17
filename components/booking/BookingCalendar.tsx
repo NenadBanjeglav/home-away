47a913cfb1b266c516c8839480b10a3ebe6a6d87
NenadBanjeglav <nenadmobmail@gmail.com>
2024-08-17 21:58:22 +0200

    booking container form calendar confirmbookingaction

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+"use client";
+
+import {
+  defaultSelected,
+  generateBlockedPeriods,
+  generateDateRange,
+  generateDisabledDates,
+} from "@/utils/calendar";
+import React, { useEffect, useState } from "react";
+import { DateRange } from "react-day-picker";
+import { Calendar } from "../ui/calendar";
+import { useProperty } from "@/utils/store";
+import { useToast } from "../ui/use-toast";
 
 const BookingCalendar = () => {
-  return <div>BookingCalendar</div>;
+  const currentDate = new Date();
+  const { toast } = useToast();
+
+  const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
+  const bookings = useProperty((state) => state.bookings);
+  const blockedPeriods = generateBlockedPeriods({
+    bookings,
+    today: currentDate,
+  });
+
+  const unavailableDates = generateDisabledDates(blockedPeriods);
+
+  useEffect(() => {
+    const selectedRange = generateDateRange(range);
+
+    const isDisabledDateIncluded = selectedRange.some((date) => {
+      if (unavailableDates[date]) {
+        setRange(defaultSelected);
+        toast({
+          description: "Some dates are booked. Please select again",
+        });
+        return true;
+      }
+      return false;
+    });
+
+    useProperty.setState({ range });
+  }, [range]);
+
+  return (
+    <Calendar
+      mode="range"
+      defaultMonth={currentDate}
+      selected={range}
+      onSelect={setRange}
+      className="mb-4"
+      disabled={blockedPeriods}
+    />
+  );
 };
 
 export default BookingCalendar;
--- components/booking/BookingContainer.tsx
+++ components/booking/BookingContainer.tsx
@@ -1,7 +1,22 @@
+"use client";
+
+import { useProperty } from "@/utils/store";
 import React from "react";
+import BookingForm from "./BookingForm";
+import ConfirmBooking from "./ConfirmBooking";
 
 const BookingContainer = () => {
-  return <div>BookingContainer</div>;
+  const { range } = useProperty((state) => state);
+
+  if (!range || !range.from || !range.to) return null;
+  if (range.to.getTime() === range.from.getTime()) return null;
+
+  return (
+    <div className="w-full">
+      <BookingForm />
+      <ConfirmBooking />
+    </div>
+  );
 };
 
 export default BookingContainer;
